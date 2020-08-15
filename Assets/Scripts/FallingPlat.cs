2da9e741f39de7be0777a9801c2a0e6a22ce01d6
rianta9 <hoangquockhanh122@gmail.com>
2020-08-16 02:22:11 +0700

    update layout, add trap & fix logic err

---
+++ Assets/Scripts/FallingPlat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlat : MonoBehaviour
+{
+    public Rigidbody2D r2;
+    public float timeDelay = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2 = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D another)
+    {
+        if (another.collider.CompareTag("Player"))
+        {
+            StartCoroutine(fall());
+        }
+    }
+
+    IEnumerator fall()
+    {
+        yield return new WaitForSeconds(timeDelay);
+        r2.bodyType = RigidbodyType2D.Dynamic;
+        yield return 0;
+    }
+}
